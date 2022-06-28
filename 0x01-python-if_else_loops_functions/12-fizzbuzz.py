2647809b7568338253674b34a478f208b1bd911a
wellyomosh <wellyomosh@gmail.com>
2022-06-28 10:10:14 -0700

    My if python commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 5 is 0 and i % 3 is 0:
-            word = "FizzBuzz"
-        elif i % 5 is 0:
-            word = "Buzz"
-        elif i % 3 is 0:
-            word = "Fizz"
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end='')
+        elif i % 3 == 0:
+            print("Fizz", end='')
+        elif i % 5 == 0:
+            print("Buzz", end='')
         else:
-            word = str(i)
-        print("{:s}".format(word), end=" ")
+            print(i, end='')
+
+        print(" ", end='')
