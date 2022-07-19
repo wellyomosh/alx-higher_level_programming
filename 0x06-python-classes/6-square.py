c8011e42eda606ac6cc68737680ea4e3160bb6a2
wellyomosh <wellyomosh@gmail.com>
2022-07-19 05:36:40 -0700

    My python class commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,60 +1,60 @@
 #!/usr/bin/python3
 class Square:
+    __size = None
+    __position = None
+
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
-        self.__position = position
-        if type(self.__position) != tuple or len(self.__position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif any(type(v) != int for v in self.__position):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif any(v < 0 for v in self.__position):
-            raise TypeError("position must be a tuple of 2 positive integers")
-
-        if not isinstance(self.__size, int):
-            raise TypeError("size must be an integer")
-        elif self.__size < 0:
-            raise ValueError("size must be >= 0")
+        if type(size) != int:
+            raise Exception("size must be an integer")
+        if size < 0:
+            raise Exception("size must be >= 0")
+        if (type(position[0]) != int or type(position[1]) != int):
+            raise Exception("position must be a tuple of 2 positive integers")
+        if len(position) != 2 or type(position) != tuple:
+            raise Exception("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise Exception("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
 
     def area(self):
-        return self.__size ** 2
+        return (self.__size * self.__size)
 
-    @property
-    def size(self):
+    def set_size(self, value):
+        if type(value) != int:
+            raise Exception("size must be an integer")
+        self.__size = value
+
+    def get_size(self):
         return self.__size
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        elif isinstance(value, int):
-            self.__size = value
+    size = property(get_size, set_size)
 
-    @property
-    def position(self):
+    def set_position(self, value):
+        if (type(value[0]) != int or type(value[1]) != int):
+            raise Exception("position must be a tuple of 2 positive integers")
+        if len(value) != 2 or type(value) != tuple:
+            raise Exception("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise Exception("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def get_position(self):
         return self.__position
 
-    @position.setter
-    def position(self, value):
-        if type(value) != tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif any(type(v) != int for v in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif any(v < 0 for v in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+    position = property(get_position, set_position)
 
     def my_print(self):
-        if self.__size != 0:
-            for k in range(self.__position[1]):
-                print()
-
-            for i in range(self.__size):
-                for l in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
-        else:
+        if self.__size == 0:
             print()
+        else:
+            for j in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(' ', end='')
+                for i in range(self.__size):
+                    print('#', end='')
+                print()
