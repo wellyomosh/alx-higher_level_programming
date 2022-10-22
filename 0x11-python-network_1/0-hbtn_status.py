908e7a69a6c16f9a9fd0739d1b93d6b935e4a794
wellyomosh <wellyomosh@gmail.com>
2022-10-21 17:04:49 -0700

    My network 2 python commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
-!/usr/bin/python3
-""" A Python script that fetches
-https://alx-intranet.hbtn.io/status
-"""
-
-import urllib.request
-
+#!/usr/bin/python3
+"""script that fetches 'https://alx-intranet.hbtn.io/status' """
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        page = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(page)))
-        print("\t- content: {}".format(page))
-        print("\t- utf8 content: {}".format(page.decode('utf-8')))
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
+        html = response.read()
+    print("Body response:")
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
